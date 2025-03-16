d9f40d19f58327e3a266de7a057db27b4ac47818
Bhavesh Mali <80244760+bhavesh14319@users.noreply.github.com>
2025-03-16 20:08:29 +0530

    Update axios.js

--- social-frontend/src/axios.js
+++ social-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://blue-enthusiastic-cobra.cyclic.app/api/v1",
+  baseURL: "https://friends-media-backend.vercel.app/api/v1",
 });
 
 export default instance
